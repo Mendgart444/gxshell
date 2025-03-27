227fa9b82760a6825b062616c8ee533734e245ee
mendgart444 <raffael.hauschildt@gmx.ch>
2025-03-27 14:54:47 +0100

    Upadated CI Scripts

--- update_macos.bat
+++
@@ -1,2 +0,0 @@
-set CARGO_TARGET_DIR=E:\gxshell_release\mac
-cargo build --release --target=x84_64-unknown-linux
--- update_win.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-echo Download GXShell...
-set CARGO_TARGET_DIR=E:\gxshell_release\win
- 
-
-cargo build --release
-cargo install --path . --root %CARGO_TARGET_DIR% 
